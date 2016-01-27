b2670bdd7bdea8c8ff62eaf13ae85b1c7ff6e58b
Juan Gallego-Calderon <jfgallego2@gmail.com>
2016-01-27 20:44:53 +0100

    Increment the counter
    It was an infinite loop!
    Also, I changed the variable name

--- take-a-break/main.py
+++ take-a-break/main.py
@@ -5,8 +5,8 @@ import time
 url = 'https://www.youtube.com/watch?v=HtcLGfKnRVw'
 
 # Initializes the counter for the number of times to repet the sequence
-counter = 1;
-while counter < 4:
+number_of_breaks = 1;
+while number_of_breaks < 4:
     time.sleep(10)
     webbrowser.open(url)
-    
+    number_of_breaks = number_of_breaks + 1
