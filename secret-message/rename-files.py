4ce5deb2421b69bb7cfecaed5f555beb8b9639f3
Juan Gallego-Calderon <jfgallego2@gmail.com>
2016-01-27 21:44:44 +0100

    Added pictures for mini-project

---
+++ secret-message/rename-files.py
@@ -0,0 +1,14 @@
+import os
+def rename_files(folder):
+    # (1) get file names from a folder
+    file_list = os.listdir(folder)
+    print file_list
+    saved_path = os.getcwd()
+    # (2) for each file, rename file
+    os.chdir(folder)
+    for file_name in file_list:
+        os.rename(file_name,file_name.translate(None,"0123456789"))
+    os.chdir(saved_path)
+    
+Photo_folder = "/Users/jk_home/Documents/Juan/Code/udacity/Programming-Foundations-with-Python-/secret-message/prank"
+rename_files(Photo_folder)
