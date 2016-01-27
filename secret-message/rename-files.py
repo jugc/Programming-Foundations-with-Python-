168888e603188a97b75965907daf8dbd4a29ed26
Juan Gallego-Calderon <jfgallego2@gmail.com>
2016-01-27 21:58:21 +0100

    Mini-project: secret message complete
    - Rename the files to make the message messy
    - Saved a copy of the files with the messy names

--- secret-message/rename-files.py
+++ secret-message/rename-files.py
@@ -9,6 +9,7 @@ def rename_files(folder):
     for file_name in file_list:
         os.rename(file_name,file_name.translate(None,"0123456789"))
     os.chdir(saved_path)
-    
-Photo_folder = "/Users/jk_home/Documents/Juan/Code/udacity/Programming-Foundations-with-Python-/secret-message/prank"
-rename_files(Photo_folder)
+
+#Photo_folder = "/Users/jk_home/Documents/Juan/Code/udacity/Programming-Foundations-with-Python-/secret-message/prank"
+photo_folder_mini_proj = "/Users/jk_home/Documents/Juan/Code/udacity/Programming-Foundations-with-Python-/secret-message/my-secret-message"
+rename_files(photo_folder_mini_proj)
