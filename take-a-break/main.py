be7f79b07b23e5f74ce970a39f5bdf2ef69ff5cb
Juan Gallego-Calderon <jugc@dtu.dk>
2016-01-28 08:46:09 +0100

    Changed units of break time in minutes

--- take-a-break/main.py
+++ take-a-break/main.py
@@ -7,9 +7,9 @@ url = 'https://www.youtube.com/watch?v=HtcLGfKnRVw'
 # Initializes the counter for the number of times to repet the sequence
 number_of_breaks = 0
 total_breaks = 3
-work_time = 5       # in seconds
+work_time = 45       # in minutes
 print "This program started on "+time.ctime()
 while number_of_breaks < total_breaks:
-    time.sleep(work_time)
+    time.sleep(work_time*60)
     webbrowser.open(url)
     number_of_breaks = number_of_breaks + 1
